3ee5c4d8f11773bd62d78eda53c40e72712b4ac7
mamadjavaad <mamadjavad8237@gmail.com>
2023-12-18 17:08:48 +0330

    updating nav 🚀

---
+++ src/components/Navbar.vue
@@ -0,0 +1,32 @@
+<script setup>
+// toggle theme
+import { useTheme } from 'vuetify'
+const theme = useTheme()
+const toggleTheme = ()=>{
+  theme.global.name.value = theme.global.current.value.dark ? 'light' : 'dark'
+}
+</script>
+
+<template>
+<nav class="w-100 d-flex flex-column flex-wrap pa-5 border-b">
+
+  <div class="w-100  bg-transparent d-flex justify-space-between align-center ">
+    <a href="https://github.com/mamadjavaad" target="_blank">
+      <v-btn icon="mdi-github" variant="tonal"></v-btn>  
+    </a>
+   <div class="d-flex flex-column text-center">
+    <a href="/" target="_blank" class="font-weight-bold " >
+      <v-btn prepend-icon="mdi-food-drumstick" variant="tonal">رزرو غذا</v-btn>  
+    </a>
+   </div>
+    <v-btn icon="mdi-theme-light-dark"  @click="toggleTheme()" variant="tonal"></v-btn>
+  </div>
+
+</nav>
+ 
+</template>
+<style>
+.v-input .v-label {
+  font-size: 0.8em;
+}
+</style>
