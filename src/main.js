7598c6af3b0198d1c13899b8a357915c651790f6
mamadjavaad <mamadjavad8237@gmail.com>
2023-12-18 17:08:23 +0330

    initial commit ✔️

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import './assets/base.css'
+import 'vuetify/styles'
+import '@mdi/font/css/materialdesignicons.css' 
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import vuetify from './utils/vuetify'
+const app = createApp(App)
+
+app.use(router)
+app.use(vuetify)
+app.mount('#app')
---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Navbar from '../components/Navbar.vue'
+</script>
+
+<template>
+  <v-layout class="d-flex flex-column flex-wrap">
+    <Navbar />
+
+
+  </v-layout>
+</template>
