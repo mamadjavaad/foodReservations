722812765958bf17bfc211de8c40b8da73584c05
mamadjavaad <mamadjavad8237@gmail.com>
2024-01-17 16:27:53 +0330

    Add user panel layout ➕

---
+++ src/layouts/UserPanel.vue
@@ -0,0 +1,81 @@
+<script setup>
+import Navbar from '@/components/Navbar.vue';
+import { ref ,onMounted} from 'vue'
+const drawer = ref(null)
+// toggle theme
+import useThemeStore from '@/store/theme'
+const { setTheme, toggleTheme } = useThemeStore();
+import { useTheme } from 'vuetify'
+const theme = useTheme()
+
+onMounted(() => {
+
+  setTheme(theme)
+})
+</script>
+<template >
+        <Navbar>
+            <template v-slot:nav-left>
+                <v-btn icon="mdi-theme-light-dark" @click="toggleTheme(theme)" variant="tonal"></v-btn>
+            </template>
+            <template v-slot:nav-center>
+                <a href="/panel" class="font-weight-bold ">
+                    <v-btn prepend-icon="mdi-home-account" variant="tonal">پنل کاربری</v-btn>
+                </a>
+            </template>
+            <template v-slot:nav-right>
+                <v-app-bar-nav-icon variant="tonal" @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+            </template>
+
+        </Navbar>
+<v-layout class="v-flex flex-column">
+
+        <v-navigation-drawer location="right" v-model="drawer" >
+            <template v-slot:prepend>
+                <v-list-item lines="three" title="محمدجواد فخرایی" subtitle="40118537">
+                    <template v-slot:prepend>
+                        <v-btn variant="text" icon="mdi-chevron-left" @click.stop="drawer = !drawer"></v-btn>
+                    </template>
+                </v-list-item>
+
+            </template>
+
+            <v-divider></v-divider>
+
+            <v-list density="compact" nav>
+                <!-- <v-list-item prepend-icon="mdi-home-city" title="داشبورد" value="home"></v-list-item> -->
+                <v-list-item prepend-icon="mdi-account" title="پروفایل من" value="account"></v-list-item>
+
+                <v-list-group value="Food">
+                    <template v-slot:activator="{ props }">
+                        <v-list-item v-bind="props" prepend-icon="mdi-food" title="رزرو غذا"></v-list-item>
+                    </template>
+
+                    <v-list-item value="weekly" title="رزرو هفتگی" prepend-icon="mdi-calendar-week"></v-list-item>
+                    <v-list-item value="daily" title="روز فروش" prepend-icon="mdi-sun-clock-outline"></v-list-item>
+                    <v-list-item value="forgotReceipt" title="کد فراموشی" prepend-icon="mdi-lock-question"></v-list-item>
+
+
+                </v-list-group>
+
+                <v-list-group value="finance">
+                    <template v-slot:activator="{ props }">
+                        <v-list-item prepend-icon="mdi-cash-fast "  v-bind="props" title="امکانات مالی"></v-list-item>
+                    </template>
+                    <v-list-item value="increaseCredit" title="افزایش اعتبار" prepend-icon="mdi-cash-multiple"></v-list-item>
+                    <v-list-item value="transferCredit" title="انتقال اعتبار" prepend-icon="mdi-bank-transfer-out"></v-list-item>
+                    <v-list-item value="financeHistory" title="تاریخچه مالی" prepend-icon="mdi-clipboard-text-clock-outline"></v-list-item>
+
+
+                </v-list-group>
+                <v-list-item prepend-icon="mdi-exit-to-app" title="خروج" value="exit"></v-list-item>
+
+
+            </v-list>
+        </v-navigation-drawer>
+        <div>
+            ..
+        </div>
+    </v-layout>
+</template>
+
--- src/router/index.js
+++ src/router/index.js
@@ -9,11 +9,11 @@ const router = createRouter({
       name: 'home',
       component: HomeView
     },
-    // {
-    //   path: '/about',
-    //   name: 'about',
-    //   component: () => import('../views/AboutView.vue')
-    // }
+    {
+      path: '/panel',
+      name: 'panel',
+      component: () => import('../layouts/UserPanel.vue'),
+    }
   ]
 })
 
